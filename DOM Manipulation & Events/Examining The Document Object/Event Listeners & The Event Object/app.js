c5e05bbb27d4aa479f4d4f9f98ff1422cc41672a
Peter <peter.zgurovets@gmail.com>
2022-09-29 14:05:55 -0700

    Add files via upload

---
+++ DOM Manipulation & Events/Examining The Document Object/Event Listeners & The Event Object/app.js
@@ -0,0 +1,48 @@
+
+// document.querySelector('.clear-tasks').addEventListener('click', function (e) {
+//     console.log('Hello World');
+
+//     e.preventDefault(); //prevent defaul behavior.
+// });
+
+
+document.querySelector('.clear-tasks').addEventListener('click', function onClick(e) {
+
+    // console.log('Clicked');
+
+    let val;
+
+    val = e;
+
+
+    // Event target element
+
+    val = e.target;
+    val = e.target.id;
+    val = e.target.className;
+    val = e.target.classList;
+
+    e.target.innerText = 'Hello';
+
+    val = e.type;  // shows type of event
+
+
+    // Timestamp
+
+    val = e.timeStamp;
+
+
+
+    //Coords event relative to the window.
+    val = e.clientY;
+    val = e.clientX;
+
+    //Coords event relative to the element.
+    val = e.offsetY;
+    val = e.offsetX;
+
+
+    console.log(val);
+
+
+});

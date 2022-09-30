708840e1d5feef0ca6eb7d3ed3b5fa4739c42d8f
Peter <peter.zgurovets@gmail.com>
2022-09-29 17:24:43 -0700

    Add files via upload

---
+++ DOM Manipulation & Events/Examining The Document Object/Event Bubbling & Delegation/app.js
@@ -0,0 +1,47 @@
+// Event Bubbling
+
+document.querySelector('.card-title').addEventListener('click', function () {
+    console.log('card-title');
+});
+
+
+document.querySelector('.card-content').addEventListener('click', function () {
+    console.log('card-contens');
+});
+
+document.querySelector('.card').addEventListener('click', function () {
+    console.log('card');
+});
+
+document.querySelector('.col').addEventListener('click', function () {
+    console.log('col');
+});
+
+
+
+
+//EVENT DELEGATION
+
+const delItem = document.querySelector('.delete-item');
+
+delItem.addEventListener('click', deleteItem);
+
+
+
+document.body.addEventListener('click', deleteItem);
+
+
+function deleteItem(e) {
+
+    // if (e.target.parentElement.className === 'delete-item secondary-content') {
+    //     console.log('delete item');
+    // }
+
+
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        console.log('delete item');
+
+        e.target.parentElement.parentElement.remove();
+    }
+
+}
